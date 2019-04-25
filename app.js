4f4620d02abf5360ddbfd2210d0c1d01c7f665d7
Ren Mingrui <972931182@qq.com>
2019-04-25 23:08:11 +0800

    1.修复package启动脚本一处笔误
    2.增加通过nodejs调用webpack的手动错误处理
    
    todo:
    1.详细修改readme
    
    2.合并到master

--- app.js
+++ app.js
@@ -26,7 +26,25 @@ const webpack = require('webpack')
 const complier = webpack(webpackConfig)
 
 if (process.env.NODE_ENV === 'production') {
-  complier.run()
+  complier.run((err, stats) => {
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
+    }
+  
+    const info = stats.toJson();
+  
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+  
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
+  })
 } else{
   app.use(webpackDevMiddleware(complier, {
     publicPath: '/'
